2bee9d6bdcdcbd5ea74e06ef54a92d161ecbb53c
Aaditee Thorat <aaditeethorat@gmail.com>
2026-06-29 11:47:35 +0530

    Fix backticks in console log

--- Task-1/index.js
+++ Task-1/index.js
@@ -17,5 +17,5 @@ app.post('/submit', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log('Server running at http://localhost:${PORT}');
+    console.log(`Server running at http://localhost:${PORT}`);
 });
