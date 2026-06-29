d021f30c17fb067fcbcd18acb4738b617ae1b730
Aaditee Thorat <aaditeethorat@gmail.com>
2026-06-29 11:23:38 +0530

    Organize Task 1 into separate folder

---
+++ Task-1/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => { 
+    res.render('index');    
+});
+
+app.post('/submit', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    res.render('result', { name: name, email: email });
+});
+
+app.listen(PORT, () => {
+    console.log('Server running at http://localhost:${PORT}');
+});
